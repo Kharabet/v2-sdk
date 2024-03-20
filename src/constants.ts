a2ab18a9316efa0ac232a9c96e6e8f4ee965856c
Rodion Kharabet <pensioneer4ik@gmail.com>
2024-03-20 20:49:51 +0200

    add crossfi chain

--- src/constants.ts
+++ src/constants.ts
@@ -8,7 +8,8 @@ export enum ChainId {
   ROPSTEN = 3,
   RINKEBY = 4,
   GÖRLI = 5,
-  KOVAN = 42
+  KOVAN = 42,
+  CROSSFI = 4157
 }
 
 export enum TradeType {
@@ -22,9 +23,9 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0xA3e9BF36Ff51cE14A25a2cF4b4086CBcf1df781B'
 
-export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+export const INIT_CODE_HASH = '0x97fe61384d0aed6f145eff143d6773691f987c4147f3a1503ed6305a96d87a56'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
--- src/entities/token.ts
+++ src/entities/token.ts
@@ -79,5 +79,6 @@ export const WETH = {
     'Wrapped Ether'
   ),
   [ChainId.GÖRLI]: new Token(ChainId.GÖRLI, '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 18, 'WETH', 'Wrapped Ether'),
-  [ChainId.KOVAN]: new Token(ChainId.KOVAN, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH', 'Wrapped Ether')
+  [ChainId.KOVAN]: new Token(ChainId.KOVAN, '0xd0A1E359811322d97991E03f863a0C30C2cF029C', 18, 'WETH', 'Wrapped Ether'),
+  [ChainId.CROSSFI]: new Token(ChainId.CROSSFI, '0x28cC5eDd54B1E4565317C3e0Cfab551926A4CD2a', 18, 'XFI', 'XFI')
 }
